6a2a792fe0eb98dc15cb2bdb4b30f0911ca5a5d8
voyager-1 <cyhzero@gmail.com>
2023-01-05 23:28:26 +0800

    update: iterator

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,27 @@ impl<T> List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
 
+impl<T> List<T> {
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.pop_back()
+    }
+}
 
 
 #[cfg(test)]
@@ -204,6 +224,22 @@ mod tests {
 
         assert_eq!(*list.peek_back().unwrap(), 1);
         assert_eq!(*list.peek_back_mut().unwrap(), 1);
+    }
+
+    #[test]
+    fn into_iter_works() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next_back(), None);
+
     }
 
 }
