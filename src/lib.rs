a759e5af53c30b12c177b7b0273d697e5e194841
voyager-1 <cyhzero@gmail.com>
2023-01-07 00:48:37 +0800

    finish

--- src/lib.rs
+++ src/lib.rs
@@ -66,71 +66,92 @@ impl<T> Drop for Stack<T> {
     }
 }
 
+pub struct List<T> {
+    left: Stack<T>,
+    right: Stack<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self {
+            left: Stack::new(),
+            right: Stack::new(),
+        }
+    }
+
+    pub fn push_left(&mut self, elem: T) {
+        self.left.push(elem);
+    }
+
+    pub fn push_right(&mut self, elem: T) {
+        self.right.push(elem);
+    }
+
+    pub fn pop_left(&mut self) -> Option<T> {
+        self.left.pop()
+    }
+
+    pub fn pop_right(&mut self) -> Option<T> {
+        self.right.pop()
+    }
+
+    pub fn peek_left(&self) -> Option<&T> {
+        self.left.peek()
+    }
+
+    pub fn peek_right(&self) -> Option<&T> {
+        self.right.peek()
+    }
+
+    pub fn peek_left_mut(&mut self) -> Option<&mut T> {
+        self.left.peek_mut()
+    }
+
+    pub fn peek_right_mut(&mut self) -> Option<&mut T> {
+        self.right.peek_mut()
+    }
+
+    // 左边的元素出栈，push到右边的栈
+    pub fn go_left(&mut self) -> bool {
+        self.left.pop_node().map(|node| {
+            self.right.push_node(node);
+        }).is_some()
+    }
+
+    pub fn go_right(&mut self) -> bool {
+        self.right.pop_node().map(|node| {
+            self.left.push_node(node);
+        }).is_some()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn basics_work() {
-        let mut list = List::new();
-        assert_eq!(list.pop(), None);
+        let mut list: List<i32> = List::new();
 
-        list.push(1);
-        list.push(2);
-        list.push(3);
+        list.push_left(0);                      // [0 |   ]
+        list.push_right(1);                     // [0 | 1]
+        assert_eq!(list.peek_left(), Some(&0));
+        assert_eq!(list.peek_right(), Some(&1));
 
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), Some(2));
-    }
+        list.push_left(2);                      // [0, 2 | 1]
+        list.push_left(3);                      // [0, 2, 3 | 1]
+        list.push_right(4);                     // [0, 2, 3 | 4, 1]
 
-    #[test]
-    fn into_iter_works() {
-        let mut list = List::new();
-        assert_eq!(list.pop(), None);
+        while list.go_left() {} // 把左栈的所有元素都移到右栈
 
-        // 注：这里的push是从尾部push的
-        list.push(1);
-        list.push(2);
-        list.push(3);
+        assert_eq!(list.pop_left(), None);          // [ | 0, 2, 3, 4, 1]
+        assert_eq!(list.pop_right(), Some(0));      // [ | 2, 3, 4, 1]
+        assert_eq!(list.pop_right(), Some(2));      // [ | 3, 4, 1]
 
-        let mut iter = list.into_iter();
-        assert_eq!(iter.next(), Some(1));
-        assert_eq!(iter.next(), Some(2));
-        assert_eq!(iter.next(), Some(3));
-        assert_eq!(iter.next(), None);
-    }
-
-    #[test]
-    fn iter_works() {
-        let mut list = List::new();
-        assert_eq!(list.pop(), None);
-
-        // 注：这里的push是从尾部push的
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        let mut iter = list.iter();
-        assert_eq!(iter.next(), Some(&1));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&3));
-        assert_eq!(iter.next(), None);
-    }
-
-    #[test]
-    fn iter_mut_works() {
-        let mut list = List::new();
-        assert_eq!(list.pop(), None);
-
-        // 注：这里的push是从尾部push的
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        let mut iter = list.iter_mut();
-        assert_eq!(iter.next(), Some(&mut 1));
-        assert_eq!(iter.next(), Some(&mut 2));
-        assert_eq!(iter.next(), Some(&mut 3));
-        assert_eq!(iter.next(), None);
+        list.push_left(5);                     // [5 | 3, 4, 1]
+        assert_eq!(list.pop_right(), Some(3));      // [5 | 4, 1]
+        assert_eq!(list.pop_left(), Some(5));       // [  | 4, 1]
+        assert_eq!(list.pop_right(), Some(4));      // [  | 1]
+        assert_eq!(list.pop_right(), Some(1));      // [     ]
     }
 }
