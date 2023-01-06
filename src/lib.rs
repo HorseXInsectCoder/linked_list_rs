fb60b0c7267bdfc809e48bea531dba60ec5c3f24
voyager-1 <cyhzero@gmail.com>
2023-01-06 19:20:45 +0800

    update: unsafe implement link-list and iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,135 +1,54 @@
+use std::ptr;
 
-use std::cell::{Ref, RefCell, RefMut};
-use std::ops::Deref;
-use std::rc::Rc;
-use std::sync::Arc;
-
-type Link<T> = Option<Rc<RefCell<Node<T>>>>;
-
-struct List<T> {
-    head: Link<T>,
-    tail: Link<T>,
-}
+type Link<T> = Option<Box<Node<T>>>;
 
 struct Node<T> {
     elem: T,
     next: Link<T>,
-    prev: Link<T>,
 }
 
-impl<T> Node<T> {
-    fn new(elem: T) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Self {
-            elem,
-            next: None,
-            prev: None
-        }))
-    }
+struct List<T> {
+    head: Link<T>,
+    tail: *mut Node<T>,
 }
 
 impl<T> List<T> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             head: None,
-            tail: None
+            tail: ptr::null_mut()        // 由于tail的类型是指针，所以不能直接用None
         }
     }
 
-    fn push_front(&mut self, elem: T) {
-        // let node = Rc::new(RefCell::new(Node {
-        //     elem,
-        //     next: None,
-        //     prev: None
-        // }));
+    // 从尾部push进去
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node {
+            elem,
+            next: None,
+        });
+        let raw_tail: *mut _ = &mut *new_tail;      // 自动推断为&mut Node<T>类型会无法编译，要手动加上 *mut _
 
-        let node = Node::new(elem);
-
-        match self.head.take() {
-            Some(head) => {
-                head.borrow_mut().prev = Some(node.clone());
-                node.borrow_mut().next = Some(head);
-                self.head = Some(node);
-            },
-            None => {
-                self.tail = Some(node.clone());
-                self.head = Some(node.clone());
-            },
-        }
-    }
-
-    fn pop_front(&mut self) -> Option<T> {
-        self.head.take().map(|node: Rc<RefCell<Node<T>>>| {
-            // match node.borrow_mut().next.take() {
-            //     Some(next) => {
-            //         next.borrow_mut().prev = None;
-            //         self.head = Some(next);
-            //     },
-            //     None => {
-            //         self.tail.take();
-            //     }
-            // }
-
-            if let Some(next) = node.borrow_mut().next.take() {
-                next.borrow_mut().prev = None;
-                self.head = Some(next);
-            } else {
-                self.tail.take();
+        if !self.tail.is_null() {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
             }
-
-            Rc::try_unwrap(node).ok().unwrap().into_inner().elem
-        })
-    }
-
-    fn peek_front(&self) -> Option<Ref<T>> {
-        self.head.as_ref().map(|node: &Rc<RefCell<Node<T>>>| {
-            Ref::map(node.borrow(), |node| {
-                &node.elem
-            })
-        })
-    }
-
-    fn push_back(&mut self, elem: T) {
-        let node = Node::new(elem);
-        if let Some(tail_node) = self.tail.take() {
-            tail_node.borrow_mut().next = Some(node.clone());
-            node.borrow_mut().prev = Some(tail_node);
-
-            // 这一步是必须的，把指针指到最后一个元素
-            self.tail = Some(node);
         } else {
-            self.head = Some(node.clone());
-            self.tail = Some(node);
+            self.head = Some(new_tail);
         }
+        self.tail = raw_tail;
     }
 
-    fn pop_back(&mut self) -> Option<T> {
-        self.tail.take().map(|node: Rc<RefCell<Node<T>>>| {
-            if let Some(prev_node) = node.borrow_mut().prev.take() {
-                prev_node.borrow_mut().next.take();
-                self.tail = Some(prev_node);
-            } else {
-                self.head.take();
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            let head = *node;
+            self.head = head.next;
+
+            // 由于上面那步已经把要pop的节点的下一个元素被self.head指向了，所以要检查这个元素是否为空
+            // 如果该元素为空，那么self这个位置就给它一个尾指针
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
             }
-
-            Rc::try_unwrap(node).ok().unwrap().into_inner().elem
-        })
-    }
-
-    fn peek_back(&self) -> Option<Ref<T>> {
-        self.tail.as_ref().map(|node: &Rc<RefCell<Node<T>>>| {
-            Ref::map(node.borrow(), |node| &node.elem)
-        })
-    }
-
-    fn peek_front_mut(&mut self) -> Option<RefMut<T>> {
-        self.head.as_mut().map(|node: &mut Rc<RefCell<Node<T>>>| {
-            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
-        })
-    }
-
-    fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
-        self.tail.as_mut().map(|node: &mut Rc<RefCell<Node<T>>>| {
-            RefMut::map(node.borrow_mut(), |node| &mut node.elem)
+            head.elem
         })
     }
 }
@@ -137,7 +56,7 @@ impl<T> List<T> {
 pub struct IntoIter<T>(List<T>);
 
 impl<T> List<T> {
-    fn into_iter(self) -> IntoIter<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
 }
@@ -146,16 +65,55 @@ impl<T> Iterator for IntoIter<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.pop_front()
+        self.0.pop()
     }
 }
 
-impl<T> DoubleEndedIterator for IntoIter<T> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        self.0.pop_back()
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref()
+        }
     }
 }
 
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut()
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -164,82 +122,64 @@ mod tests {
     #[test]
     fn basics_work() {
         let mut list = List::new();
-        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.pop(), None);
 
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
-        assert_eq!(list.pop_front(), Some(3));
-        assert_eq!(list.pop_front(), Some(2));
-
-        list.push_front(4);
-        list.push_front(5);
-
-        assert_eq!(list.pop_front(), Some(5));
-        assert_eq!(list.pop_front(), Some(4));
-        assert_eq!(list.pop_front(), Some(1));
-        assert_eq!(list.pop_front(), None);
-    }
-
-    #[test]
-    fn peek_works() {
-        let mut list = List::new();
-        // assert_eq!(list.peek_front(), None);     binary operation `==` cannot be applied to type `Option<Ref<'_, _>>`
-        assert!(list.peek_front().is_none());
-
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
-
-        assert_eq!(*list.peek_front().unwrap(), 3);
-    }
-
-    #[test]
-    fn pop_back_works() {
-        let mut list = List::new();
-        assert_eq!(list.pop_back(), None);
-
-        list.push_back(4);
-        list.push_back(5);
-        assert_eq!(list.pop_back(), Some(5));
-        assert_eq!(list.pop_back(), Some(4));
-        assert_eq!(list.pop_back(), None);
-    }
-
-    #[test]
-    fn peek_front_back_works() {
-        let mut list = List::new();
-        assert!(list.peek_front().is_none());
-        assert!(list.peek_back().is_none());
-        assert!(list.peek_front_mut().is_none());
-        assert!(list.peek_back_mut().is_none());
-
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
-
-        assert_eq!(*list.peek_front().unwrap(), 3);
-        assert_eq!(*list.peek_front_mut().unwrap(), 3);
-
-        assert_eq!(*list.peek_back().unwrap(), 1);
-        assert_eq!(*list.peek_back_mut().unwrap(), 1);
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
     }
 
     #[test]
     fn into_iter_works() {
         let mut list = List::new();
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
+        assert_eq!(list.pop(), None);
+
+        // 注：这里的push是从尾部push的
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
         let mut iter = list.into_iter();
-        assert_eq!(iter.next(), Some(3));
-        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(3));
         assert_eq!(iter.next(), None);
-        assert_eq!(iter.next_back(), None);
-
     }
 
+    #[test]
+    fn iter_works() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        // 注：这里的push是从尾部push的
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut_works() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        // 注：这里的push是从尾部push的
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 1));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), None);
+    }
 }
